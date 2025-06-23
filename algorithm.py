c003da3f961f3f62af041f85a6eccd1cc891d629
gmzklz <131411318+gmzklz@users.noreply.github.com>
2025-06-23 19:11:16 +0300

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,35 @@
+import heapq
+from collections import Counter
 
+class Node:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+def build_huffman_tree(text):
+    frequency = Counter(text)
+    heap = [Node(char, freq) for char, freq in frequency.items()]
+    heapq.heapify(heap)
+
+    while len(heap) > 1:
+        n1 = heapq.heappop(heap)
+        n2 = heapq.heappop(heap)
+        merged = Node(None, n1.freq + n2.freq)
+        merged.left = n1
+        merged.right = n2
+        heapq.heappush(heap, merged)
+
+    return heap[0]
+
+def build_codes(node, prefix='', code_map={}):
+    if node:
+        if node.char is not None:
+            code_map[node.char] = prefix
+        build_codes(node.left, prefix + '0', code_map)
+        build_codes(node.right, prefix + '1', code_map)
+    return code_map
